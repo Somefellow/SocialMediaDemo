2b65c242be0ba4b8d1d4f3aa0efd3cdddc00d2d2
Aidas Domasevicius <aidas_dom@gmx.com>
2022-12-25 21:35:32 +1100

    More updates to test script

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+export PATH=$PATH:$HOME/.rvm/bin
 export RAILS_ENV=test
 export RACK_ENV=test
 
+rvm use 2.7.7
+
 bin/bundle install
 bin/rake assets:precompile
-rake db:reset
+bin/rake db:reset
 bin/rake test
