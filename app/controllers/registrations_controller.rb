d4cec9f0d726293b8c7125641597807010054f86
Aidas Domasevicius <aidas_dom@gmx.com>
2022-12-27 15:47:53 +1100

    Create registrations

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class RegistrationsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.create(email: params[:registration][:email],
+                       full_name: params[:registration][:full_name],
+                       password: params[:registration][:password])
+    if user
+        flash[:success] = 'Successful registration. You may now login.'
+        redirect_to root_path
+    else
+        flash[:danger] = 'Email address already registered.'
+        render :new
+    end
+  end
+end
