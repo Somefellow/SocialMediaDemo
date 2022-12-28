c80f8d2db7bc8af2af0f5fe1c27321dd86848c58
Aidas Domasevicius <aidas_dom@gmx.com>
2022-12-28 19:09:20 +1100

    Create registration controller tests

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,9 +4,17 @@ class RegistrationsController < ApplicationController
   def new; end
 
   def create
+    flash[:danger] = 'Email address required.' unless params[:registration][:email].present?
+    flash[:danger] = 'Full name required.' unless params[:registration][:full_name].present?
+    flash[:danger] = 'Password required.' unless params[:registration][:password].present?
+    render :new && return unless params[:registration][:email].present? &&
+                                 params[:registration][:full_name].present? &&
+                                 params[:registration][:password].present?
+
     user = User.create(email: params[:registration][:email],
                        full_name: params[:registration][:full_name],
                        password: params[:registration][:password])
+
     if user
       flash[:success] = 'Successful registration. You may now login.'
       redirect_to root_path
--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -3,7 +3,43 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get registration' do
+    get register_path
+    assert_response :success
+  end
+
+  test 'should create the user on registration' do
+    post register_path, params: { registration: { email: 'david_heinemeier_hansson@example.com', full_name: 'David Heinemeier Hansson', password: 'password1' } }
+    assert User.find_by(email: 'david_heinemeier_hansson@example.com').present?
+    User.destroy_all
+  end
+
+  test 'should direct to home page on registration' do
+    post register_path, params: { registration: { email: 'david_heinemeier_hansson@example.com', full_name: 'David Heinemeier Hansson', password: 'password1' } }
+    follow_redirect!
+    assert_equal flash[:success], 'Successful registration. You may now login.'
+    assert_equal path, root_path
+    User.destroy_all
+  end
+
+  test 'should require an email on register' do
+    user_count = User.count
+    post register_path, params: { registration: { full_name: 'David Heinemeier Hansson', password: 'password1' } }
+    assert_equal flash[:danger], 'Email address required.'
+    assert_equal user_count, User.count
+  end
+
+  test 'should require full name on register' do
+    user_count = User.count
+    post register_path, params: { registration: { email: 'david_heinemeier_hansson@example.com', password: 'password1' } }
+    assert_equal flash[:danger], 'Full name required.'
+    assert_equal user_count, User.count
+  end
+
+  test 'should require a password on register' do
+    user_count = User.count
+    post register_path, params: { registration: { email: 'david_heinemeier_hansson@example.com', full_name: 'David Heinemeier Hansson' } }
+    assert_equal flash[:danger], 'Password required.'
+    assert_equal user_count, User.count
+  end
 end
